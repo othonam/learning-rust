84255f578876088913cd3c345b3b897ce9388726
Othon A. Menezes <othonam@live.com>
2022-12-12 23:53:04 -0300

    implementing http listener and tcp reader

--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     addr: String,
@@ -14,9 +15,19 @@ impl  Server {
 
         let listener = TcpListener::bind(&self.addr).unwrap();
 
-        while true {
-            
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer))
+                        },
+                        Err(e) => print!("Failed to read from connection: {}", e)
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e)
+            }
         }
-
     }
 }
