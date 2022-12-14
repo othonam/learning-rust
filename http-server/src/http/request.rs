ea2b8b62b7021f2b2dc90cd1a1a375ff195da483
Othon A. Menezes <othonam@live.com>
2022-12-13 23:57:23 -0300

    including parseErrorEnum, functions and traits for parseError. Including function to read route and parameters.

--- http-server/src/http/request.rs
+++ http-server/src/http/request.rs
@@ -1,4 +1,9 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Formatter, Display, Result as FmtResult, Debug};
+use std::str::Utf8Error;
+use std::str;
 
 pub struct Request {
     path: String,
@@ -6,3 +11,74 @@ pub struct Request {
     method: Method,
 
 }
+
+// impl Request{
+//     fn from_byte_array(buf: &[u8]) -> Result<Self, String>{}
+// }
+
+impl TryFrom<&[u8]> for Request{
+    type Error = ParseError;
+
+    //GET /search?name=abc&sort=1 HTTP/1.1
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        let request = str::from_utf8(buf)?;
+
+        match get_next_word(request){
+            Some((method, request)) => {},
+            None => return  Err(ParseError::InvalidRequest),
+        }
+
+        get_next_word(request).ok_or(ParseError::InvalidRequest);
+
+        unimplemented!()
+    }
+}
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' {
+            return Some((&request[..i], &request[(i+1)..]))
+        }
+    }
+
+    None
+}
+
+pub enum ParseError{
+    InvalidRequest,
+    InvalidEnconding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str{
+        match self {
+            Self::InvalidRequest=>"Invalid Request",
+            Self::InvalidEnconding=>"Invalid Enconding",
+            Self::InvalidProtocol=>"Invalid Protocol",
+            Self::InvalidMethod=>"Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError{
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEnconding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Error for ParseError {}
--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,5 +1,9 @@
+use std::io::Result;
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::request::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     addr: String,
@@ -22,6 +26,11 @@ impl  Server {
                     match stream.read(&mut buffer){
                         Ok(_) => {
                             print!("Received a request: {}", String::from_utf8_lossy(&buffer))
+
+                           match Request::try_from(&buffer[..]) {
+                               Ok(request) => {},
+                               Err(e) => print!("Failed to parse a request: {}", e)
+                           }
                         },
                         Err(e) => print!("Failed to read from connection: {}", e)
                     }
